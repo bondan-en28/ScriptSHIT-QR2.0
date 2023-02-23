3a38a8f8b25d8774517ae7a4f35750270fe9d2fb
bondan-en28 <bondanekanugraha28@gmail.com>
2023-02-23 23:18:36 +0700

    win-refactor

--- cv/qr_target_identifier.py
+++ cv/qr_target_identifier.py
@@ -14,7 +14,7 @@ color_pink = 153,51,255
 
 def readQRs(frame):
     qr_codes = pyzbar.decode(frame)
-    recognized_qr_qount = 0
+    recognized_qr_count = 0
     identified_qr_dict= recognized_qr_dict = None
 
     for qr_code in qr_codes: #UNTUK SETIAP QRCODE YANG ADA DI DALAM FRAME
@@ -34,18 +34,18 @@ def readQRs(frame):
         if id and lat and lon is not None:
             cv2.rectangle(frame, (x, y),(x+w, y+h), color_green, 2)
             recognized_qr_dict = qr_dict
-            recognized_qr_qount+=1
+            recognized_qr_count+=1
         else:
             cv2.rectangle(frame, (x, y),(x+w, y+h), color_red, 2)
 
         cv2.putText(frame, qr_data, (x + 6, y - 6), font, 0.5, color_pink, 1)
     
-    if recognized_qr_qount<1:
+    if recognized_qr_count<1:
         cv2.putText(frame, "Waiting for QR...", (10, int(cam_height-20)), font, 0.5, color_green, 1)
-    if recognized_qr_qount==1:
+    if recognized_qr_count==1:
         cv2.putText(frame, "QR Identified>>"+str(recognized_qr_dict['id']), (10, int(cam_height-20)), font, 0.5, color_green, 1)
         identified_qr_dict = recognized_qr_dict
-    elif recognized_qr_qount>1:
+    elif recognized_qr_count>1:
         cv2.putText(frame, "Multiple QR's Detected, It's CONFUSING!", (10, int(cam_height-20)), font, 0.5, color_green, 1)
 
     returnValue = [frame, identified_qr_dict]
@@ -61,22 +61,22 @@ def main():
     cam_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     ret, frame = camera.read()
-    identifiedCounter = 0
-    identifiedQRDict = None
+    identified_counter = 0
+    identified_qr_dict = None
     print("Here main function @qr_target_identifier.py")
 
     while ret:
         ret, frame = camera.read()
-        frame, identifiedQRDict= readQRs(frame)
-        if identifiedQRDict is not None:
-            identifiedCounter+=1
-            cv2.line(frame, (0, int(cam_height-5)), (int(identifiedCounter/50*cam_width) , int(cam_height-5)), color_green, 3)
+        frame, identified_qr_dict= readQRs(frame)
+        if identified_qr_dict is not None:
+            identified_counter+=1
+            cv2.line(frame, (0, int(cam_height-5)), (int(identified_counter/50*cam_width) , int(cam_height-5)), color_green, 3)
         cv2.putText(frame, "Frame: "+ str(cam_width)+"x" + str(cam_height),(10,20),font, 0.5, color_green, 1) #resolusi frame
         cv2.imshow('Target Identifier', frame)
-        if cv2.waitKey(1) & identifiedCounter>50:
+        if cv2.waitKey(1) & identified_counter>50:
             break
 
     camera.release()
     print("Released Camera.")
     cv2.destroyAllWindows()
-    return identifiedQRDict
+    return identified_qr_dict
